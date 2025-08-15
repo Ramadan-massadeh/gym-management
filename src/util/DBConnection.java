1f155f66bcda1fca56d0a27961da3f5c96b871e1
Campbell Kramer <campbellkramer@gmail.com>
2025-08-15 05:21:42 -0230

    Added postgreSQL database connection functionality

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,6 +1,21 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
-    
+    private static final String URL = "jdbc:postgresql://localhost:5432/gymdb";
+    private static final String USER = "student";
+    private static final String PASSWORD = ":^(((";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver"); // loads the driver
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL JDBC Driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
